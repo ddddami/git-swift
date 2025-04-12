4c32bf636e130a9cfd72af70da57da2e2a6c96a0
Damilola O. <82976159+ddddami@users.noreply.github.com>
2025-04-12 21:57:51 +0100

    Display git branches in fuzzy finder

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+
+	"github.com/ktr0731/go-fuzzyfinder"
+)
+
+type Branch struct {
+	Name      string
+	IsCurrent bool
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	cmd := exec.Command("git", "branch")
+	out, _ := cmd.Output()
+	branches := strings.Split(string(out), "\n")
+
+	idx, err := fuzzyfinder.Find(
+		branches, func(i int) string {
+			return branches[i]
+		},
+	)
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+	}
+	fmt.Printf("selected: %v\n", idx)
 }
